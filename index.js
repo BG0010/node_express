7abf4703d0d851a1eda033fd62dce2e805be1aa4
Vinícius Borges <borgesvini11@gmail.com>
2023-10-10 14:17:39 -0300

    recebendo parâmetros na rota

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const path = require("path")
 const app = express()
 
 const caminhoBase = path.join(__dirname, "templates") 
+// https://localhost:3000/usuarios
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    // resposta.sendFile(`${caminhoBase}/usuarios.html`)
+    const id = requisicao.params.id
+    console.log(`Acessando dados do usuário negro do id: ${id}`)
+
+})
 
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
