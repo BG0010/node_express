db42e0b3e895b75a40d2280ce7ecc0be85f042f4
Vinícius Borges <borgesvini11@gmail.com>
2023-10-30 15:50:00 -0300

    inicio do projeto - cadastrar

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates") 
 // https://localhost:3000/usuarios
+
+
+
 app.get('/usuarios/:id', (requisicao, resposta) => {
     // resposta.sendFile(`${caminhoBase}/usuarios.html`)
     const id = requisicao.params.id
